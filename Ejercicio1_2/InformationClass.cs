71d4e933133dafba84ddb07be07da44c52af49fc
JReniery <JReniery@DESKTOP-1PCHB5J>
2022-06-21 14:50:32 -0600

    Add project files.

---
+++ Ejercicio1_2/InformationClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2
+{
+    public class InformationClass
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSend_Clicked(object sender, EventArgs e)
+        {
+            var Info = new InformationClass
+            {
+                Name = txbName.Text,
+                LastName = txbLastName.Text,
+                Age = Convert.ToInt16(txbAge.Text),
+                Email = txbEmail.Text
+            };
+            var InfoPage = new ViewDataPage
+            {
+                BindingContext = Info
+            };
+            await Navigation.PushAsync(InfoPage);
+        }
+    }
+}
